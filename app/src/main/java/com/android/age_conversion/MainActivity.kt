a09a03c61ca42430bcb57e664a32f1f439799f2d
Ren <coffeeaxce@gmail.com>
2021-01-09 17:36:47 +0900

    猫の年齢変換機

---
+++ app/src/main/java/com/android/age_conversion/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.android.age_conversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val year_edit: EditText = findViewById(R.id.year_edit)
+        val month_edit: EditText = findViewById(R.id.month_edit)
+        val conv_button: Button = findViewById(R.id.conv_button)
+
+        conv_button.setOnClickListener{
+            Log.println(Log.ASSERT,"year_edit",year_edit.text.toString())
+
+            val year_text:Int = Integer.parseInt(year_edit.text.toString())
+            val month_text:Int = Integer.parseInt(month_edit.text.toString())
+            var age : Int = 0
+
+
+            if(year_text == 0){
+                if(month_text < 12 )
+                    when(month_text){
+                        1 -> age = 1
+                        2 -> age = 3
+                        in 3..5 -> age = 5
+                        in 6..8 -> age = 9
+                        in 9..11 -> age = 13
+                    }
+
+                Log.println(Log.ASSERT,"age",age.toString())
+
+            }
+            else if(year_text == 1){
+                if(month_text < 6){
+                    age = 17
+                }
+                else if(month_text >= 6 && month_text < 12){
+                    age = 20
+                }
+            }
+            else if(year_text == 2){
+                age = 23
+            }
+            else if(year_text >= 3){
+                age = 28 + (4*(year_text-3))
+            }
+
+            val man_text:TextView = findViewById(R.id.man_edit)
+            man_text.text = age.toString()
+        }
+    }
+}
