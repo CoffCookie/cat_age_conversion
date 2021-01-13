7b46732eab842115c4f328ce4563251c8aa768c7
Ren <coffeeaxce@gmail.com>
2021-01-13 16:42:55 +0900

    猫クラス作成

---
+++ app/src/main/java/com/android/age_conversion/CatCalc.kt
@@ -0,0 +1,36 @@
+package com.android.age_conversion
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class CatCalc(var year_text: Int,var month_text:Int, var age:Int) {
+
+    fun age_calc() {
+        if (year_text == 0) {
+            if (month_text < 12)
+                when (month_text) {
+                    1 -> age = 1
+                    2 -> age = 3
+                    in 3..5 -> age = 5
+                    in 6..8 -> age = 9
+                    in 9..11 -> age = 13
+                }
+
+            Log.println(Log.ASSERT, "age", age.toString())
+
+        } else if (year_text == 1) {
+            if (month_text < 6) {
+                age = 17
+            } else if (month_text >= 6 && month_text < 12) {
+                age = 20
+            }
+        } else if (year_text == 2) {
+            age = 23
+        } else if (year_text >= 3) {
+            age = 28 + (4 * (year_text - 3))
+        }
+    }
+}
--- app/src/main/java/com/android/age_conversion/MainActivity.kt
+++ app/src/main/java/com/android/age_conversion/MainActivity.kt
@@ -17,49 +17,24 @@ class MainActivity : AppCompatActivity() {
         val conv_button: Button = findViewById(R.id.conv_button)
 
         conv_button.setOnClickListener{
-
-
             var year_text:Int = 0
             var month_text:Int = 0
-            var age : Int = 0
+            var age:Int = 0
 
-            if(year_edit.length() != 0) {
+            if (year_edit.length() != 0) {
                 year_text = Integer.parseInt(year_edit.text.toString())
             }
-            if(month_edit.length() != 0){
+            if (month_edit.length() != 0) {
                 month_text = Integer.parseInt(month_edit.text.toString())
             }
 
-            if(year_text == 0){
-                if(month_text < 12 )
-                    when(month_text){
-                        1 -> age = 1
-                        2 -> age = 3
-                        in 3..5 -> age = 5
-                        in 6..8 -> age = 9
-                        in 9..11 -> age = 13
-                    }
+            val cat_calc = CatCalc(year_text,month_text,age)
+            cat_calc.age_calc()
+            age = cat_calc.age
 
-                Log.println(Log.ASSERT,"age",age.toString())
-
-            }
-            else if(year_text == 1){
-                if(month_text < 6){
-                    age = 17
-                }
-                else if(month_text >= 6 && month_text < 12){
-                    age = 20
-                }
-            }
-            else if(year_text == 2){
-                age = 23
-            }
-            else if(year_text >= 3){
-                age = 28 + (4*(year_text-3))
-            }
-
-            val man_text:TextView = findViewById(R.id.man_edit)
+            val man_text: TextView = findViewById(R.id.man_edit)
             man_text.text = age.toString()
+
         }
     }
 }
